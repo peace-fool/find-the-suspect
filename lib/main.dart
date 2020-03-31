d9d0d909238ab29ba7000b2d9d7ef343671415fb
utkc137-codeverse <utkarsh.learn2020@gmail.com>
2020-03-31 18:43:30 +0530

    setState done / interface is yet to be improved

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,10 @@ class MyAppState extends State<MyApp>{
   var s2 = ['s2> I was not there', 's2> I told you I was not there!, goddamn it!!', 's2> WTF bro, It wasen\'t me', 's2> hehe ;)'];
   var s3 = ['s3> This is so stupid', 's3> Hey dude wtf!', 's3> Stop it', 's3> What a drag...'];
   var si1=0,si2=0,si3 = 0;
-  sf1(){
-    if((si2==0)&& (si3==0)){
+
+     sf1(){
+       setState(() {
+          if((si2==0)&& (si3==0)){
       si1 = 1;
       si2 = 3;
       si3 = 3;
@@ -27,27 +29,49 @@ class MyAppState extends State<MyApp>{
     else if (si1 == 1){
       si1 = 2;
     }
+    else {
+      si1 = 2;
+      si2 = 3;
+      si3 = 3;
+    }
+       });
+   
   }
   sf2(){
-     if((si1==0)&& (si3==0)){
+    setState(() {
+       if((si1==0)&& (si3==0)){
       si2 = 1;
       si1 = 3;
       si3 = 3;
     }
-    else if (si1 == 1){
+    else if (si2 == 1){
       si2 = 2;
     }
+    else {
+      si1 = 3;
+      si2 = 2;
+      si3 = 3;
+    }
+    });
+    
 
   }
   sf3(){
-     if((si2==0)&& (si1==0)){
+    setState(() {
+       if((si2==0)&& (si1==0)){
       si3 = 1;
       si2 = 3;
       si1 = 3;
     }
-    else if (si1 == 1){
+    else if (si3 == 1){
       si3 = 2;
     }
+    else {
+      si1 = 3;
+      si2 = 3;
+      si3 = 2;
+    }
+    });
 
   }
   @override
@@ -57,7 +81,7 @@ class MyAppState extends State<MyApp>{
         title: Text('Guess the suspect'),
       ),
       body: Column(children: [
-        Text('the person you suspect 3 times is going to be convicted!'),
+        Text('The person you suspect in second round is going to be convicted!'),
         Text('Choose Wisely'),
         RaisedButton(child: Text(s1[si1]) ,onPressed: sf1),
         RaisedButton(child: Text(s2[si2]) ,onPressed: sf2),
